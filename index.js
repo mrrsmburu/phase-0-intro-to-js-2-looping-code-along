96b0bb01489bc59ef2c24eef69e459c37db442a7
mrrsmburu <mrrsmburu@gmail.com>
2023-10-08 17:07:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 
 function writeCards(names, event) {
-  const messages = [];
+  const messages = []
   for (let i = 0; i < names.length; i++) {
-    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
-  return messages;
+  return messages
 }
-
-
 function countDown(number) {
   while (number >= 0) {
-    console.log(number);
+    console.log(number)
     number--;
+
   }
 }
